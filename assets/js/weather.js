2f8114c88b312f8236816646e38ad4feb9677313
Cadulo <celopez28@outlook.com>
2023-03-05 21:34:30 -0500

    Cargar datos semanales de otras ciudades

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,36 +1,51 @@
-import { weather_data } from './data.js';
+import { weather_data } from "./data.js";
 
-const selectbutton=document.getElementById("dropdownMenuButton")
+const selectbutton = document.getElementById("dropdownMenuButton");
+const loadbutton = document.getElementById("loadinfo");
+let city = 0;
+const loadDayForecastData = (e, city = 0) => {
+  let identificadores = [
+    "city",
+    "date",
+    "maxtemperature",
+    "mintemperature",
+    "cloudiness",
+    "wind",
+    "rainfall",
+  ];
 
-const loadDayForecastData = (e,city=0) => {
-    let identificadores=["city","date","maxtemperature","mintemperature","cloudiness","wind","rainfall"]
-   
-    for (let e of identificadores){
-        document.getElementById(`${e}`).innerHTML = weather_data[city][`${e}`]
-    }
-    let lateId=["late_temperature","late_forecast","late_text","late_icon"]
-    let nightId=["night_temperature","night_forecast","night_text","night_icon"]
-    let dayKey=["temperature","forecast","text","icon"]
+  for (let e of identificadores) {
+    document.getElementById(`${e}`).innerHTML = weather_data[city][`${e}`];
+  }
+  let lateId = ["late_temperature", "late_forecast", "late_text", "late_icon"];
+  let nightId = [
+    "night_temperature",
+    "night_forecast",
+    "night_text",
+    "night_icon",
+  ];
+  let dayKey = ["temperature", "forecast", "text", "icon"];
 
-    for(let i=0; i<dayKey.length;i++){
-        document.getElementById(`${lateId[i]}`).innerHTML = weather_data[city]["forecast_today"][0][`${dayKey[i]}`]
-        document.getElementById(`${nightId[i]}`).innerHTML=weather_data[city]["forecast_today"][1][`${dayKey[i]}`]
-    }
-    
-}
+  for (let i = 0; i < dayKey.length; i++) {
+    document.getElementById(`${lateId[i]}`).innerHTML =
+      weather_data[city]["forecast_today"][0][`${dayKey[i]}`];
+    document.getElementById(`${nightId[i]}`).innerHTML =
+      weather_data[city]["forecast_today"][1][`${dayKey[i]}`];
+  }
+};
 
-const loadWeekForecastData = (e,city=0) => {
-  
-    const week = document.querySelector(".list-group")
-    week.innerHTML=" "
-    let len=weather_data[0]["forecast_week"].length
-    for (let i = 0; i < len; i++) {
-        let text= weather_data[0]["forecast_week"][i]["text"]
-        let date= weather_data[0]["forecast_week"][i]["date"]
-        let icon= weather_data[0]["forecast_week"][i]["icon"]
-        let min= weather_data[0]["forecast_week"][i]["temperature"]["min"]
-        let max= weather_data[0]["forecast_week"][i]["temperature"]["max"]
-        let prediction=`  <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
+const loadWeekForecastData = (e, city = 0) => {
+  const week = document.querySelector(".list-group");
+  console.log(city)
+  week.innerHTML = " ";
+  let len = weather_data[0]["forecast_week"].length;
+  for (let i = 0; i < len; i++) {
+    let text = weather_data[city]["forecast_week"][i]["text"];
+    let date = weather_data[city]["forecast_week"][i]["date"];
+    let icon = weather_data[city]["forecast_week"][i]["icon"];
+    let min = weather_data[city]["forecast_week"][i]["temperature"]["min"];
+    let max = weather_data[city]["forecast_week"][i]["temperature"]["max"];
+    let prediction = `  <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg">
         <div class="d-flex flex-column">
           <h6 class="mb-1 text-dark font-weight-bold text-sm" >${text}</h6>
           <span class="text-xs" >${date}</span>
@@ -40,23 +55,24 @@ const loadWeekForecastData = (e,city=0) => {
           <div class="ms-4"><i class="material-icons fs-2 me-1 rainy" >${icon}</i></div>
         </div>
       </li>
-      `
-      week.innerHTML+=prediction
-    }	
-}
+      `;
+    week.innerHTML += prediction;
+  }
+};
 
-window.addEventListener("DOMContentLoaded",loadDayForecastData)
-document.getElementById("loadinfo").addEventListener("click",loadWeekForecastData)
+window.addEventListener("DOMContentLoaded", loadDayForecastData);
 
-selectbutton.addEventListener("click",(e)=>{
-    console.log(selectbutton)
-    let city=0
-    if (e.target.value=="Guayaquil"){
-        city=0
-    }else if (e.target.value=="Ambato"){
-        city=1
-    }else if (e.target.value=="Tena"){
-        city=2
-}
-    loadDayForecastData(null,city)
-})
+selectbutton.addEventListener("click", (e) => {
+  if (e.target.value == "Guayaquil") {
+    city = 0;
+  } else if (e.target.value == "Ambato") {
+    city = 1;
+  } else if (e.target.value == "Tena") {
+    city = 2;
+  }
+  loadDayForecastData(null, city);
+});
+
+loadbutton.addEventListener("click", (e) => {
+  loadWeekForecastData(null, city);
+});
